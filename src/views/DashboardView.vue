642988c3948797af44e64df0731be9feda2ad4b2
Andy Fusniak <andy@andyfusniak.com>
2020-06-29 21:04:15 +0700

    Vuetify 2.3.2

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container fluid>
-    <v-content class="mx-4 pb-4">
+    <v-main class="mx-4 pb-4">
       <c-navbar class="pb-4"></c-navbar>
 
       <router-view name="dashboard"></router-view>
-    </v-content>
+    </v-main>
   </v-container>
 </template>
 
