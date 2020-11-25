539f43aef260939c040ad4696f10a2bd50ae85ab
Andy Fusniak <andy@andyfusniak.com>
2020-11-25 14:44:43 +0000

    Deps update

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# CHANGELOG
+
+## 0.1.0 (25 Nov 2020)
++ Firebase 8
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import * as firebase from 'firebase/app'
-import "firebase/auth"
+import firebase from 'firebase/app'
+import 'firebase/auth'
 import capturoo from './capturoo-client'
 
 (async () => {
